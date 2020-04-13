e7577f92863e8010f7d0a2b5e6c38df97a118e52
DavidAbram <david@abram.com>
2020-04-13 17:58:36 +0200

    updated algorithm

--- index.js
+++ index.js
@@ -1,56 +1,14 @@
 let result = [];
 
-function subsetSet(
-  input = [
-    {
-      name: '1',
-      value: 1.1,
-    },
-    {
-      name: '2',
-      value: 2.2,
-    },
-    {
-      name: '3',
-      value: 3.3,
-    },
-    {
-      name: '4',
-      value: 4.4,
-    },
-    {
-      name: '5',
-      value: 5.5,
-    },
-  ],
-  target = 5.3,
-  tolerance = 0.5,
-  partial = [],
-) {
-  const sum = partial.reduce((total, n) => total + n.value, 0);
-  
-  if(sum > target + tolerance) return;
 
-  if(sum >= target - tolerance && sum <= target + tolerance) {
-    partial.sort((a, b) => a.name - b.name);
-    if(!result.find(t => JSON.stringify(t.result) === JSON.stringify(partial))) {
-      result.push({
-        sum,
-        result: partial,
-        diff: Math.abs(target - sum),
-      });
-    }
-  }
-
-  input.filter(n => !partial.includes(n)).forEach(n => {
-    subsetSet(input, target, tolerance, [...partial, n]);
-  });
-}
 
 function calculate() {
   result = [];
   let inputValue = document.querySelector('#main-input').value;
+  const tolerance = Number(document.querySelector('#error-tolerance').value);
+  const target = Number(document.querySelector('#target').value);
   
+
   if(inputValue === '') {
     inputValue = [
       {
@@ -75,14 +33,46 @@ function calculate() {
       },
     ];
   } else {
-    const numbers = inputValue.split(/\r?\n/);
+    const numbers = inputValue.split(/\r?\n/).filter(t => t);
     inputValue = numbers.map((n, i) => ({
       name: i.toString(),
       value: Number(n),
     }));
   }
-  
-  
+
+  for (let index = 0; index < 2**inputValue.length; index++) {
+    const mask = dec2bin(index).padStart(inputValue.length, "0").split('').map(t => Number(t) ? true : false);
+    let partial = inputValue.filter((t, i) => mask[i]);
+    let sum = partial.reduce((total, p) => total + p.value, 0);
+
+    if(sum > target + tolerance) {
+      continue;
+    };
+
+    if(sum >= target - tolerance && sum <= target + tolerance) {
+      if(!result.find(t => JSON.stringify(t.result) === JSON.stringify(partial))) {
+        result.push({
+          sum,
+          result: partial,
+          diff: Math.abs(target - sum),
+        });
+      }
+    }
+  }
+  result.sort((a, b) => a.diff - b.diff);
+
+  const resultInput = document.querySelector('#results');
+  const fixed = (n) => (Math.round(n * 100) / 100).toFixed(2);
+  const resultString = result.reduce((total, r) => {
+    total += `sum: ${fixed(r.sum)}\n`;
+    r.result.forEach((n) => {
+      total += `index ${n.name} (value: ${fixed(n.value)})\n`;
+    });
+    total += `------------------------\n\n`;
+    return total;
+  }, '');
+  resultInput.value = resultString;
+  /*
   const errorTolerance = Number(document.querySelector('#error-tolerance').value);
   const target = Number(document.querySelector('#target').value);
   
@@ -100,5 +90,12 @@ function calculate() {
     total += `------------------------\n\n`;
     return total;
   }, '');
-  resultInput.value = resultString;
+  resultInput.value = resultString;*/
+}
+
+
+
+
+function dec2bin(dec){
+  return (dec >>> 0).toString(2);
 }
